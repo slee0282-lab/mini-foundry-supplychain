4738e0b78f0bbf1fe10437c9a77f55178a5c947e
Seungho Lee <slee0282@student.monash.edu>
2025-09-25 15:32:29 +1000

    feat: add the chart

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
 def main():
-    print("Hello from mini-foundry-supplychain!")
+    st.title("My Supply Chain Dashboard")
+    st.write("Hello World!")
+
+    # Your charts/graphs go here
+    data = pd.DataFrame({'x': [1,2,3], 'y':
+    [1,4,9]})
+    st.line_chart(data)
 
 
 if __name__ == "__main__":
